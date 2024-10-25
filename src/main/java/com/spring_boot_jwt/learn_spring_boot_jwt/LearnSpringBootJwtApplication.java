93a699e415a58fc25c8ff19fef274a790aefe13b
Rafiklatrikdu69 <rafik.bouchenna050@email.com>
2024-10-25 13:53:36 +0200

    jwt learn

---
+++ src/main/java/com/spring_boot_jwt/learn_spring_boot_jwt/LearnSpringBootJwtApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_jwt.learn_spring_boot_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot_jwt/learn_spring_boot_jwt/LearnSpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_jwt.learn_spring_boot_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
